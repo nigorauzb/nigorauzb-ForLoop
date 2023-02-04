4ea8972c32769cbd05a7aa83aedb4dc3aa448d49
nigorauzb <pittsburgh.decor@gmail.com>
2023-02-04 17:10:49 +0000

    commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -42,7 +42,7 @@ public class Lab {
     public String returnNumbers(int start, int end){
         String buildingString = "";
         for (int i = start; i < end; i++){
-            buildingString +=i; 
+            buildingString = buildingString+i+" "; 
         }
 
 //        code here
